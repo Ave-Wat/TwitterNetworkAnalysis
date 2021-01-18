68da750b06176d1cf41a1424ceb94190a87a1224
aokib <aokib@carleton.edu>
2021-01-17 23:24:06 -0600

    tweepy example

--- modi_network_tweepy.py
+++ modi_network_tweepy.py
@@ -1,10 +1,12 @@
 import tweepy
 import tokens
+import time
 
 auth = tweepy.OAuthHandler(tokens.consumer_key, tokens.consumer_secret)
 auth.set_access_token(tokens.access_token, tokens.access_token_secret)
 
 api = tweepy.API(auth)
+modi = 'narendramodi'
 
 # link on handling rate limits with cursors:
 # https://github.com/tweepy/tweepy/blob/master/docs/code_snippet.rst#pagination
@@ -20,16 +22,17 @@ def limit_handled(cursor):
             time.sleep(15 * 60)
 
 def get_friends(user):
-    friend_ids = []
-    i = 0
+    #friend_ids = []
+    i=0
+    for friend in limit_handled(tweepy.Cursor(api.friends, screen_name=user).items()):
+        print(friend.screen_name)
+        # i+=1
+        # if i > 4:
+        #     break
 
-    cursor = limit_handled(tweepy.Cursor(api.friends(user)).items())
-    for friend in cursor:
-        if i < 10:
-            print(friend.screen_name)
-        else:
-            break
-        i = i + 1
+def main():
+    user = modi
+    get_friends(modi)
 
-user = 'narendramodi'
-get_friends(user)
+if __name__ == '__main__':
+    main()
