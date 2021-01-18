dd6edcc89cef4388160efa20e92b8d1b446ef441
Ave-Wat <wattsa2@carleton.edu>
2021-01-17 21:56:25 -0600

    tried to fix pagination error

--- modi_network_tweepy.py
+++ modi_network_tweepy.py
@@ -22,7 +22,7 @@ def limit_handled(cursor):
 def get_friends(user):
     friend_ids = []
     i = 0
-    for friend in limit_handled(tweepy.Cursor(api.friends_ids, user_id = user)).items()):
+    for friend in limit_handled(tweepy.Cursor(api.friends_ids, user_id = user)).pages()):
         if i < 2:
             print(friend.user_id)
         else:
