fb62468841e0af5199669a2ba0ed017079f808da
Ave-Wat <wattsa2@carleton.edu>
2021-01-25 09:25:21 -0600

    moved practice files to practice folder. Continued working on creating a working BSoup

--- b_soup_practice.py
+++ b_soup_practice.py
@@ -1,27 +1,39 @@
 """
 python3 -m venv 'name'-env
-'name'-env\Scripts\activate.bat
+'environment name'\Scripts\activate.bat
 pip install bs4
 pip install requests
 """
 
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import csv
 
-def get_content(url):
-    headers = requests.utils.default_headers()
-    headers.update({
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-    })
+driver = webdriver.Chrome()
 
-    url = url
+headers = requests.utils.default_headers()
+headers.update({
+'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+})
+
+def get_user_page(base_url):
+    response = requests.get(base_url, headers=headers)
+    content = BeautifulSoup(response.content, "html.parser")
+    return content
+
+def get_follower_page(base_url):
+    #followers list is in <div class="css-1dbjc4n">...</div>
+    url = base_url + "/following"
     response = requests.get(url, headers=headers)
     content = BeautifulSoup(response.content, "html.parser")
     return content
 
 def main():
-    html = get_content("https://twitter.com/narendramodi?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor")
+    modi_base_url = "https://twitter.com/narendramodi"
+    html = get_follower_page(modi_base_url)
+
     print(html)
 
 if __name__ == '__main__':
