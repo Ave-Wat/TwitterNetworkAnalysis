45d96d49bb11c6fea7b270464fb7f2aca8026671
CS Lab Account <cslab@cmc306-11.mathcs.carleton.edu>
2021-02-02 22:56:49 -0600

    hrllo

--- twitterbot_class.py
+++ twitterbot_class.py
@@ -53,7 +53,8 @@ class Twitterbot:
             html = html + self.driver.page_source
 
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            scroll_pause_time = random.randint(4, 10)
+            scroll_pause_time = random.randint(6, 9)
+            scroll_pause_time = scroll_pause_time / 10
             time.sleep(scroll_pause_time)
 
             # Calculate new scroll height and compare with last scroll height
