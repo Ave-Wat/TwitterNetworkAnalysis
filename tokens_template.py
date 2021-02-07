d7c8dbb2fc24d1dab8e4c361bc00c59da6373a5b
CS Lab Account <cslab@cmc306-03.mathcs.carleton.edu>
2021-02-07 17:08:30 -0600

    updated csvs

--- tokens_template.py
+++ tokens_template.py
@@ -1,8 +1,8 @@
 
-bot_num = 1;
+bot_num = 13;
 
 def get_creds():
     pass_1 = "password1234"
     m_1 = "6162989202"
-    u_1 = 'TeaganJ15700608'
+    u_1 = 'manalyzer3'
     return pass_1, m_1, u_1
